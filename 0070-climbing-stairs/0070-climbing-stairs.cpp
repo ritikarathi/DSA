1381c9e4c8de2c93fb9261e16266ad53568c5c97
Ritika Rathi <72248682+ritikarathi@users.noreply.github.com>
2026-08-05 17:47:37 +0530

    Time: 0 ms (100%), Space: 8.7 MB (20.76%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int count(int i,int n, vector<int>&dp){
+        if(i==n){
+            return 1;
+        }
+        if(i>n){
+            return 0;
+        }
+        if(dp[i]!=-1){
+            return dp[i];
+        }
+        return dp[i]=count(i+1,n,dp)+count(i+2,n,dp);
+    }
+    int climbStairs(int n) {
+        vector<int>dp(n+2,-1);
+        return count(0,n,dp);
+    }
+};
